3f60c340efb0fdfde22794e6d45848e2ae2eba8d
Mike Vermeulen <mike@vermeulen.com>
2024-01-03 12:48:42 -0600

    Add my repository in the target.

--- docker/scripts/build_shark.sh
+++ docker/scripts/build_shark.sh
@@ -3,6 +3,11 @@
 WORKDIR=${WORKDIR:="/workdir"}
 SHARK_REPOSITORY=${SHARK_REPOSITORY:="https://github.com/nod-ai/SHARK"}
 SHARK_BRANCH=${SHARK_BRANCH:="SHARK-1.0"}
+SHARK_MEV_REPOSITORY=${SHARK_MEV_REPOSITORY:="https://github.com/mvermeulen/shark-dev"}
+
+# get a copy of my example scripts
+mkdir -p /src
+git clone --depth=1 ${SHARK_MEV_REPOSITORY}
 
 # pull in SHARK
 mkdir -p /src
