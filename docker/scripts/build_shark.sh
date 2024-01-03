69be9254bd794d6e8ac4c84b3fc66bedf1992e37
Mike Vermeulen <mike@vermeulen.com>
2024-01-03 11:42:50 -0700

    create container for saving state of SHARK build.

--- docker/scripts/build_shark.sh
+++ docker/scripts/build_shark.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 WORKDIR=${WORKDIR:="/workdir"}
+SHARK_REPOSITORY=${SHARK_REPOSITORY:="https://github.com/nod-ai/SHARK"}
 SHARK_BRANCH=${SHARK_BRANCH:="SHARK-1.0"}
 
 # pull in SHARK
+mkdir -p /src
 cd /src
-git clone --depth=1 --branch ${SHARK_BRANCH} ${SHARK_BRANCH}
+git clone --depth=1 --branch ${SHARK_BRANCH} ${SHARK_REPOSITORY} ${SHARK_BRANCH}
 cd ${SHARK_BRANCH}
 PYTHON=python3.11 ./setup_venv.sh
