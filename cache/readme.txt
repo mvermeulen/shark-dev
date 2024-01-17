59857d1055ce888305063e03a5fd7dc93701904c
Mike Vermeulen <mike@vermeulen.com>
2024-01-17 15:59:38 -0600

    Add mounted caches for run_docker.sh.

---
+++ cache/readme.txt
@@ -0,0 +1,5 @@
+Cache directories that get mounted into docker containers while running.
+
+huggingface --> /root/.cache/huggingface
+torch --> /root/.cache/torch
+shark_tank --> /root/.local/shark_tank
--- util/run_docker.sh
+++ util/run_docker.sh
@@ -3,16 +3,23 @@
 # Script pass the right parameters to run docker on ROCm
 # includes mounting /home/mev
 DOCKER=${DOCKER:="ort-migraphx:6.0"}
+CACHE=""
 
 if [ `id -u` != 0 ]; then
     echo script should be run as root
     exit 0
 fi
 
-if [ -d /extra ]; then
-    EXTRAMOUNT="-v /extra:/extra"
-else
-    EXTRAMOUNT=""
+if [ -d /home/mev/source/shark-dev/cache/shark_tank ]; then
+    CACHE="$CACHE -v /home/mev/source/shark-dev/cache/shark_tank:/root/.local/shark_tank"
 fi
 
-docker run -it -e TZ=America/Chicago -e TARGET=gpu --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev $EXTRAMOUNT $DOCKER /bin/bash
+if [ -d /home/mev/source/shark-dev/cache/huggingface ]; then
+    CACHE="$CACHE -v /home/mev/source/shark-dev/cache/huggingface:/root/.cache/huggingface"
+fi
+
+if [ -d /home/mev/source/shark-dev/cache/torch ]; then
+    CACHE="$CACHE -v /home/mev/source/cache/torch:/root/.cache/torch"
+fi
+
+docker run -it -e TZ=America/Chicago $CACHE --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev $DOCKER /bin/bash
