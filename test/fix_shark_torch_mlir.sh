05dc147b38c9aa71c9894e4ece45c7d07847a7ea
Mike Vermeulen <mike@vermeulen.com>
2024-01-18 12:28:32 -0600

    Update fix script.

--- test/fix_shark_torch_mlir.sh
+++ test/fix_shark_torch_mlir.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
-# Fix the pinned version of SHARK issue.
+# Work around the pinned version of SHARK issue.
+# Run this first and then retry the setup_venv.sh script.
 python3.11 -m pip https://github.com/llvm/torch-mlir/releases/download/snapshot-20231210.1048/torch_mlir-20231210.1048-cp311-cp311-linux_x86_64.whl
 ./setup_venv.sh
