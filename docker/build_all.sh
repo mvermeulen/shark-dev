444c20857c39477c6ff80364270582db4a0ccd9d
Mike Vermeulen <mike@vermeulen.com>
2024-01-18 13:05:45 -0600

    Add gfx906:sramecc+:xnack- for MIGraphX build.

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-TAG=${TAG:="6.0"}
+ROCM_VERSION=${ROCM_VERSION:="5.7"}
+
+# Set to "--no-cache" to rebuild the world
 CACHE=${CACHE:=""}
+
+# NOTE: SHARK-1.0 requires ROCm 5.7.
+#       MIGraphX can use ROCm 6.0
+case $ROCM_VERSION in
+    5.7)
+	TAG=${TAG:="5.7"}
+	INSTALL_SCRIPT="https://repo.radeon.com/amdgpu-install/5.7.1/ubuntu/jammy/amdgpu-install_5.7.50701-1_all.deb"
+	 ;;
+    6.0)
+	TAG=${TAG:="6.0"}
+	INSTALL_SCRIPT="https://repo.radeon.com/amdgpu-install/6.0/ubuntu/jammy/amdgpu-install_6.0.60000-1_all.deb"
+	 ;;
+esac
+
+
 DATESTAMP=`date '+%Y%m%d-%H%M'`
 
 docker build -f rocm-dev -t rocm:${TAG} ${CACHE}\
-       --build-arg install_script="https://repo.radeon.com/amdgpu-install/6.0/ubuntu/jammy/amdgpu-install_6.0.60000-1_all.deb" \
+       --build-arg install_script=${INSTALL_SCRIPT} \
        . 2>&1 | tee rocm:${TAG}.${DATESTAMP}.log
 docker tag rocm:${TAG} rocm:${DATESTAMP}
 
--- test/fix_shark_torch_mlir.sh
+++ test/fix_shark_torch_mlir.sh
@@ -2,5 +2,8 @@
 #
 # Work around the pinned version of SHARK issue.
 # Run this first and then retry the setup_venv.sh script.
-python3.11 -m pip https://github.com/llvm/torch-mlir/releases/download/snapshot-20231210.1048/torch_mlir-20231210.1048-cp311-cp311-linux_x86_64.whl
-./setup_venv.sh
+# Switch to python3.11 to do the pip install
+update-alternatives --set python3 /usr/bin/python3.11
+pip3 install https://github.com/llvm/torch-mlir/releases/download/snapshot-20231210.1048/torch_mlir-20231210.1048-cp311-cp311-linux_x86_64.whl
+PYTHON=python3.11 ./setup_venv.sh
+update-alternatives --set python3 /usr/bin/python3.10
