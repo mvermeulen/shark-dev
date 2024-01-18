470b740cc36eaa41d932c388bc48cb0441000f97
Mike Vermeulen <mike@vermeulen.com>
2024-01-18 15:53:49 -0600

    Add stable diffusion for SHARK.

---
+++ test/shark_stable_diffusion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Expects to run in the SHARK root directory
+if [ ! -f apps/stable_diffusion/scripts/main.py ]; then
+    "stable diffusion not found, are you in a SHARK root directory?"
+    exit 0
+fi
+
+python apps/stable_diffusion/scripts/main.py --app=txt2img --precision=fp16 --device=vulkan --prompt="a photograph of an astronaut riding a horse" --seed 13 --output astro_horse.jpg
