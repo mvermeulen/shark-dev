62d952ef7d6b16369b3a840819c08826d07f47ec
Mike Vermeulen <mike@vermeulen.com>
2024-01-19 13:46:48 -0600

    Update device and CUDA Version.

--- test/shark_stable_diffusion.sh
+++ test/shark_stable_diffusion.sh
@@ -6,4 +6,12 @@ if [ ! -f apps/stable_diffusion/scripts/main.py ]; then
     exit 0
 fi
 
-python apps/stable_diffusion/scripts/main.py --app=txt2img --precision=fp16 --device=rocm --prompt="a photograph of an astronaut riding a horse" --seed 13
+if [ -d /opt/rocm ]; then
+    DEVICE=rocm
+elif [ -d /usr/local/cuda ]; then
+    DEVICE=cuda
+else
+    DEVICE=cpu
+fi
+
+python apps/stable_diffusion/scripts/main.py --app=txt2img --precision=fp16 --device=$DEVICE --prompt="a photograph of an astronaut riding a horse" --seed 13
