b9d38bf71a057350ad55b02d3e860e255b022fe8
Mike Vermeulen <mike@vermeulen.com>
2024-01-19 13:57:55 -0600

    Add file to build CUDA ORT.

---
+++ docker/scripts/build_ort_cuda.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ORT_REPOSITORY=${ORT_REPOSITORY:="https://github.com/mvermeulen/onnxruntime"}
+ORT_BRANCH=${ORT_BRANCH:="mev_add_shark"}
+CUDA_VERSION=${CUDA_VERSION:="12.2"}
+WORKDIR=${WORKDIR:="/workdir"}
+
+pip3 install packaging
+
+# update cmake
+CMAKE_VERSION=3.26.6
+cd /usr/local && \
+    wget -q -O - https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz | tar zxf -
+export PATH=/usr/local/cmake-${CMAKE_VERSION}-linux-x86_64/bin:$PATH
+
+# build ONNX runtime
+cd /src
+git clone --recursive --branch ${ORT_BRANCH} --depth=1 ${ORT_REPOSITORY}
+cd onnxruntime
+./build.sh --parallel --cmake_extra_defines ONNXRUNTIME_VERSION=`cat ./VERSION_NUMBER` CMAKE_HIP_FLAGS=-Wno-deprecated-builtins --config Release --skip_tests --build_wheel --use_migraphx --use_rocm --cuda_version=${ROCM_VERSION} --cuda_home /usr/local/cuda --allow_running_as_root
+
+cp /src/onnxruntime/build/Linux/Release/dist/*.whl ${WORKDIR}
+pip3 install /src/onnxruntime/build/Linux/Release/dist/*.whl
+
+# set up python environment
+cd /src/onnxruntime/onnxruntime/python/tools/transformers
+pip3 install -r requirements.txt
+pip3 uninstall -y torch
+pip3 install torch --index-url https://download.pytorch.org/whl/nightly/rocm5.7
+pip3 install tensorflow-gpu
