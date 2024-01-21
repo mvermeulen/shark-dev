eb6ddbc7da2b4ecc5774d0353844107d27d6fd55
Mike Vermeulen <mike@vermeulen.com>
2024-01-21 06:55:46 -0600

    Update to add checkpoints, remove cache sync and make checks in web stable diffusion.

--- test/shark_web_stable_diffusion.sh
+++ test/shark_web_stable_diffusion.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #
 # Expects to run in the root shark directory
+if [ -d apps/stable_diffusion/web ]; then
+    echo "apps/stable_diffusion/web not found are you in the SHARK directory?"
+    exit 1
+fi
+
+if [ "$VIRTUAL_ENV" = "" ]; then
+    echo "VIRTUAL_ENV is empty have you done source shark1.venv/bin/activate?"
+    exit 1
+fi
+
 cd apps/stable_diffusion/web
 python index.py
---
+++ util/checkpoint_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Uses tags to save docker images on a particular day
+
+if [ `id -u` !- 0 ]; then
+    echo "script should be run as root"
+    exit 0
+fi
+
+datestamp=`date '+%Y-%m-%d'`
+
+for dockerid in rocm:5.7 rocm:6.0 shark:5.7 shark:6.0 migraphx:5.7 migraphx:6.0 ort-migraphx:5.7 ort-migraphx:6.0
+do
+    docker tag $dockerid $dockerid-${datestamp}
+done
