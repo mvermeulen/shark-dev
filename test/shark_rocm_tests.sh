74c77547766fb4699284e8043ff0c5c1253de8fe
Mike Vermeulen <mike@vermeulen.com>
2024-01-21 09:03:17 -0600

    Fix checks for the rocm_tests.

--- test/shark_rocm_tests.sh
+++ test/shark_rocm_tests.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+if [ ! -f tank/test_models.py ]; then
+    "tank/test_models.py not found, are you in the SHARK root directory?"
+fi
+
+if [ "$VIRTUAL_ENV" = "" ]; then
+    echo "VIRTUAL_ENV is empty have you done source shark1.venv/bin/activate?"
+    exit 1
+fi
+
 pytest tank/test_models.py -k "rocm"
