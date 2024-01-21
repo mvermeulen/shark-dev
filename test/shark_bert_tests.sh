73b3ae24bcd27d978ed188e89b141f1688abcc92
Mike Vermeulen <mike@vermeulen.com>
2024-01-21 13:28:37 -0600

    Make SHARK bert tests run on both platforms.

--- test/shark_bert_tests.sh
+++ test/shark_bert_tests.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-pytest tank/test_models.py -k "rocm and bert and base"
-pytest tank/test_models.py -k "rocm and bert and large"
+if [ -d /opt/rocm ]; then
+    pytest tank/test_models.py -k "rocm and bert and base"
+    pytest tank/test_models.py -k "rocm and bert and large"
+elif [ -d /usr/local/cuda ]; then
+    pytest tank/test_models.py -k "cuda and bert and base"
+    pytest tank/test_models.py -k "cuda and bert and large"    
+else
+    pytest tank/test_models.py -k "bert and base"
+    pytest tank/test_models.py -k "bert and large"    
+fi
