0e13f66e7a5ff6c4eebb5a10cbb34013cc6ba4ea
Mike Vermeulen <mike@vermeulen.com>
2024-01-22 08:23:05 -0600

    Fix up tag for TensorRT docker.

--- util/checkpoint_docker.sh
+++ util/checkpoint_docker.sh
@@ -9,7 +9,10 @@ fi
 
 datestamp=`date '+%Y-%m-%d'`
 
-for dockerid in rocm:5.7 rocm:6.0 shark:5.7 shark:6.0 migraphx:5.7 migraphx:6.0 ort-migraphx:5.7 ort-migraphx:6.0
-do
-    docker tag $dockerid $dockerid-${datestamp}
-done
+if [ -d /opt/rocm ]; then
+    for dockerid in rocm:5.7 rocm:6.0 shark:5.7 shark:6.0 migraphx:5.7 migraphx:6.0 ort-migraphx:5.7 ort-migraphx:6.0
+    do
+	docker tag $dockerid $dockerid-${datestamp}
+    done
+elif [ -d /usr/local/cuda ]; then
+    for dockerid in cuda:11 cuda:12 shark:11 shark:12 ort-cuda:11 ort-cuda:12 tensorrt:12 ort-tensorrt:12 
