9f8e4f040b3397d80ee8b1e7a0ee9c6c36857de3
Mike Vermeulen <mike@vermeulen.com>
2024-01-22 10:04:42 -0600

    Add ability to pass in the device.

--- test/shark_stable_diffusion.sh
+++ test/shark_stable_diffusion.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Expects to run in the SHARK root directory
+DEVICE=${DEVICE:=""}
+
 if [ ! -f apps/stable_diffusion/scripts/main.py ]; then
     "stable diffusion not found, are you in a SHARK root directory?"
     exit 0
@@ -11,12 +13,14 @@ if [ "$VIRTUAL_ENV" = "" ]; then
     exit 1
 fi
 
-if [ -d /opt/rocm ]; then
-    DEVICE=rocm
-elif [ -d /usr/local/cuda ]; then
-    DEVICE=cuda
-else
-    DEVICE=cpu
+if [ "$DEVICE" = "" ]; then
+    if [ -d /opt/rocm ]; then
+	DEVICE=rocm
+    elif [ -d /usr/local/cuda ]; then
+	DEVICE=cuda
+    else
+	DEVICE=cpu
+    fi
 fi
 
 python apps/stable_diffusion/scripts/main.py --app=txt2img --precision=fp16 --device=$DEVICE --prompt="a photograph of an astronaut riding a horse" --seed 13
