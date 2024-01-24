29f194caa1e49b0c07d5c1644b766131d1f471e8
Mike Vermeulen <mike@vermeulen.com>
2024-01-24 09:26:30 -0600

    Initial generation of config.cmake.

---
+++ docker/scripts/build_tvm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+mkdir -p /src/tvm/build
+cd /src/tvm/build
+
+CONFIG_CMAKE=../cmake/config.cmake
+
+if [ -d /opt/rocm ]; then
+    sed -e 's?USE_ROCM OFF?USE_ROCM ON?g' \
+	-e 's?USE_LLVM OFF?USE_LLVM /opt/rocm/llvm/bin/llvm-config?g' \
+	-e 's?USE_MIOPEN OFF?USE_MIOPEN ON?g' \
+	-e 's?USE_ROCBLAS OFF?USE_ROCBLAS ON?g' \
+	../cmake/config.cmake > config.cmake.rocm
+    CONFIG_CMAKE=config.cmake.rocm
+elif [ -d /usr/local/cuda ]; then
+    sed -e 's?USE_CUDA OFF?USE_CUDA ON?g' \
+	-e 's?USE_CUDNN OFF?USE_CUDNN ON?g' \
+	-e 's?USE_CUBLAS OFF?USE_CUBLAS ON?g' \
+	../cmake/config.cmake > config.cmake.cuda
+    CONFIG_CMAKE=config.cmake.cuda
+fi
+
+
+sed -e 's?USE_RELAY_DEBUG OFF?USE_RELAY_DEBUG ON?g' -e 's?USE_VULKAN OFF?USE_VULKAN ON?g' $CONFIG_CMAKE > config.cmake
